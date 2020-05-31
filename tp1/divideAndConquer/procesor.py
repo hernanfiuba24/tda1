16f3b970e9f93bf4a6eb2442df3a86a65469dc90
Fede Longhi <fede-longhi@users.noreply.github.com>
2020-05-31 18:40:42 -0300

    Update procesor.py

--- tp1/divideAndConquer/procesor.py
+++ tp1/divideAndConquer/procesor.py
@@ -37,90 +37,37 @@ def procesoB(content):
 
 
 
-frecuencia = {}
 def procesoC(content):
-    rotulo = None
-    candidate = majority(content)
-    if (frecuencia[candidate] > len(content)/2):
-        rotulo = candidate
-    return rotulo
+    return majority(content)
 
 def majority(L):
     if (len(L) == 1):
-        frecuencia[L[0]] = frecuencia.get(L[0], 0) + 1
         return L[0]
 
     middle = int(len(L)/2)
+    leftM = majority(L[:middle])
+    rightM = majority(L[middle:])
 
-    # print("L1", L[:middle])
-    left = majority(L[:middle])
-    # print("left", left)
-
-    # print("L2", L[middle:])
-    right = majority(L[middle:])
-    # print("right", right)
-
-    if (left == right):
-        return left
-    
-    return left if frecuencia.get(left) > frecuencia.get(right) else right
-
-
-
-
-def mergeSort(L):
-    print("merge sort")
-    if (len(L) == 1):
-        return L
-    if (len(L) == 2):
-        if (L[0] > L[1]):
-            swap(L)
-        return L
-    middle = int(len(L)/2)
-    print("--")
-    print(L[:middle])
-    L1 = mergeSort(L[:middle])
-    print(L1)
-    print("--")
-    print(L[middle:])
-    L2 = mergeSort(L[middle:])
-    print(L2)
-    print("--")
-    return merge(L1, L2)
-
-def swap(L):
-    a = L[0]
-    L[0] = L[1]
-    L[1] = a
-    return L
-
-def merge(A, B):
-    print("merge")
-    print(A)
-    print(B)
-    L = []
-    i = 0
-    j = 0
-    while i < len(A) and j < len(B): 
-        n = A[i]
-        m = B[j]
-        if n < m:
-            L.append(n)
-            i += 1
-        else:
-            L.append(m)
-            j += 1
-    if (i == len(A) - 1):
-        L.extend(B[j:])
+    if leftM == rightM:
+        return leftM
+    elif appearances(L, leftM, 0, len(L)) > len(L)/2:
+        return leftM
+    elif appearances(L, rightM, 0, len(L)) > len(L)/2:
+        return rightM
     else:
-        L.extend(A[i:])
+        return None
 
-    return L
+def appearances(L, x, i, j):
+    c = 0
+    for k in range(i,j):
+        if (L[k] == x):
+            c += 1
+    return c
 
 
 def main(process, file):
     content = readFile(file)
-    #print(majority(content))
+
     if process == 'A':
         print("procesoA")
         print(procesoA(content))
